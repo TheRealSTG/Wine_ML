6ff3f47fcaa7d29765a81ccc641db95d84c505ec
Savir <46322136+TheRealSTG@users.noreply.github.com>
2025-10-09 19:04:23 +0530

    first commit

---
+++ Wine_ML.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+from time import time
+# Allows the use of the display() for displaying DataFrames
+# Module that displays data structures in a better format
+from IPython.display import display
+
+import matplotlib.pyplot as plt
+# Makes prettier graphs and uses matplotlibs to provide better visualization
+import seaborn as sns
+
+# Import supplementary visualization code visuals.py 
+import visuals as vs
+
